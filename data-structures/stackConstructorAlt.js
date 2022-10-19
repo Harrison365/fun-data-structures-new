efec65b29b7a74c1d0c338cb4ce8525a2ca87d14
Harrison Royds <harrisonroyds@harrisons-mbp.home>
2022-10-19 10:15:34 +0100

    constructor function refactoring, more like Liam's way

---
+++ data-structures/stackConstructorAlt.js
@@ -0,0 +1,42 @@
+function Stack(maxSize = 5) {
+  // build your stack object inside this constructor function
+  const stack = Object.create(prototype);
+  stack.quantity = 0;
+  stack.storage = {};
+  stack.maxSize = maxSize;
+
+  return stack;
+}
+
+const prototype = {
+  push: function (item) {
+    if (this.quantity < this.maxSize) {
+      this.quantity++;
+      this.storage[this.quantity] = item;
+    } else {
+      console.log(`cannot add ${item}, storage is full`);
+    }
+  },
+
+  pop: function () {
+    if (Object.keys(this.storage).length > 0) {
+      const itemToReturn = this.storage[this.quantity];
+      console.log("item to return", itemToReturn);
+      delete this.storage[this.quantity];
+      this.quantity--;
+
+      return itemToReturn;
+    }
+  },
+  isEmpty: function () {
+    return this.quantity === 0;
+  },
+  isFull: function () {
+    return this.quantity === this.maxSize;
+  },
+  peek: function () {
+    return this.storage[this.quantity];
+  },
+};
+
+module.exports = Stack;
